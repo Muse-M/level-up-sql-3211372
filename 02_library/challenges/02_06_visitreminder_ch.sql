5686d0080d2d3d59a297eac6d1dd142046cc3e64
Muse-M <97995635+Muse-M@users.noreply.github.com>
2024-06-28 13:13:49 +0000

    06 TO 08 COMPLETE

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,10 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT P.PATRONID,P.EMAIL, COUNT(*)
+FROM LOANS 
+JOIN PATRONS P
+ON LOANS.PATRONID=P.PATRONID
+GROUP BY P.PATRONID
+ORDER BY COUNT(*) 
+LIMIT 9
