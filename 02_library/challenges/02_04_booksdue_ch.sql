39315cd67904179b54ad039d80baba5ac59934e7
Muse-M <97995635+Muse-M@users.noreply.github.com>
2024-06-28 12:43:26 +0000

    04 AND 05

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,12 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+
+select title, firstname,lastname,duedate
+from loans L
+join Books B
+  on b.bookid=l.bookid
+join patrons p
+  on p.patronid=l.patronid
+where duedate= '2022-07-13' and returneddate is null
--- 02_library/challenges/02_05_returnbooks_ch.sql
+++ 02_library/challenges/02_05_returnbooks_ch.sql
@@ -3,4 +3,10 @@
 -- Return date: July 5, 2022
 -- Book 1: 6435968624
 -- Book 2: 5677520613
--- Book 3: 8730298424
+-- Book 3: 5677520613
+
+update loans 
+set returneddate='2022-07-05'
+where bookid in (select bookid from books 
+                    where barcode in (6435968624,5677520613,5677520613))
+          and retuneddate is null --IMPORTANT!!!!!!!!!!!!!!!!!!
