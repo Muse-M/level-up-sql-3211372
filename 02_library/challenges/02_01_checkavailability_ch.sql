18269494c1febbf525132d4c070d634947be6f87
Muse-M <97995635+Muse-M@users.noreply.github.com>
2024-06-28 12:18:54 +0000

    01_01 to 03 correct

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,11 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+select loanid, returnedDate 
+from loans 
+join books 
+on loans.bookid=books.bookid
+where books.title ='Dracula'
+      and returnedDate is null
+
+--1 copy out of 3 not returned
--- 02_library/challenges/02_03_checkout_ch.sql
+++ 02_library/challenges/02_03_checkout_ch.sql
@@ -3,3 +3,23 @@
 -- Book 2: Great Expectations, 4043822646
 -- The checkout date is August 25, 2022 and the 
 -- due date is September 8, 2022.
+
+SELECT PATRONID 
+FROM PATRONS
+WHERE FIRSTNAME='Jack'
+
+Select bookid
+from books
+where barcode= 2855934983 or barcode=4043822646
+
+insert into loans(bookid,patronid,loandate,duedate)
+values((Select bookid
+from books
+where barcode= 2855934983),(SELECT PATRONID 
+FROM PATRONS
+WHERE FIRSTNAME='Jack'),'2022-08-25','2022-09-08'),
+      ((Select bookid
+from books
+where barcode= 4043822646),(SELECT PATRONID 
+FROM PATRONS
+WHERE FIRSTNAME='Jack'),'2022-08-25','2022-09-08')
