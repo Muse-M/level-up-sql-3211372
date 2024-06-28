d1361354bdecfa5bb72d91cba58d923767cc5e11
Muse-M <97995635+Muse-M@users.noreply.github.com>
2024-06-28 09:31:30 +0000

    01_03 done, commit using git commite -a

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,16 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+SELECT * 
+FROM DISHES 
+ORDER BY PRICE ASC
+
+SELECT * 
+FROM DISHES
+WHERE TYPE LIKE 'Appetizer' OR TYPE LIKE 'Beverage'
+
+SELECT * 
+FROM DISHES
+WHERE TYPE NOT LIKE 'Beverage'
+ORDER BY TYPE  
